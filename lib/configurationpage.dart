90786721c5eb558538f646b0e1a6975db8df4ed6
christian@voegtle.org <christian@voegtle.org>
2019-08-13 21:35:17 +0200

    Checkbox aktivieren, wenn sie geklickt wird.

--- lib/configurationpage.dart
+++ lib/configurationpage.dart
@@ -3,10 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_wetterwolke/configuration.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class ConfigurationPage extends StatelessWidget {
+class ConfigurationPage extends StatefulWidget {
   final Configuration configuration;
 
   ConfigurationPage(this.configuration);
+
+  @override
+  State<StatefulWidget> createState() => ConfigurationState(configuration);
+}
+
+class ConfigurationState extends State<ConfigurationPage> {
+  final Configuration configuration;
+
+  ConfigurationState(this.configuration);
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +24,13 @@ class ConfigurationPage extends StatelessWidget {
         body: ListView(children: configuration.locations.map((
             LocationConfiguration location) {
           return CheckboxListTile(title: Text(location.city),
-              value: location.enabled,
-              onChanged: (bool state) {
+            value: location.enabled,
+            onChanged: (bool state) {
+              setState(() {
                 _setLocationState(location.location, state);
               });
+            },
+            secondary: const Icon(Icons.location_on),);
         }).toList(),
         ));
   }
@@ -30,5 +42,7 @@ class ConfigurationPage extends StatelessWidget {
     l.location == locationId);
     location.enabled = state;
   }
+
+
 }
 

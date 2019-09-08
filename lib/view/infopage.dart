095672776404c9b9bb85b838f95753dc82258154
christian@voegtle.org <christian@voegtle.org>
2019-09-08 14:41:35 +0200

    Upgrade auf Android 10 SDK Debug Code für Probleme beim Konfigurationsabruf

--- lib/view/infopage.dart
+++ lib/view/infopage.dart
@@ -57,7 +57,7 @@ class InfoPage extends StatelessWidget {
                               "https://github.com/cvoegtle/flutter_wetterwolke/blob/master/HELP.md")),
                   Divider(),
                   Caption("Versions Information"),
-                  Text("Version 1.0.6-12 vom 08.09.2019")
+                  Text("Version 1.0.6-12 vom 08.09.2019 ")
                 ]))));
   }
 }

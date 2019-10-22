9fb5266ab9b050d0bd810807bb449cb3d198c2f1
christian@voegtle.org <christian@voegtle.org>
2019-10-22 22:54:26 +0200

    Stationen auf der Karte markieren

--- lib/view/mappage.dart
+++ lib/view/mappage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/src/layer/tile_provider/tile_provider.dart';
 import 'package:flutter_wetterwolke/data/weatherdata.dart';
+import 'package:intl/intl.dart';
 import 'package:latlong/latlong.dart';
 
 class MapPage extends StatelessWidget {
@@ -29,26 +30,26 @@ class MapPage extends StatelessWidget {
             new TileLayerOptions(
               urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
               subdomains: ['a', 'b', 'c'],
-              // For example purposes. It is recommended to use
-              // TileProvider with a caching and retry strategy, like
-              // NetworkTileProvider or CachedNetworkTileProvider
               tileProvider: CachedNetworkTileProvider(),
             ),
             new MarkerLayerOptions(
-              markers: [
-                new Marker(
-                  width: 80.0,
-                  height: 80.0,
-                  point: new LatLng(51.5, -0.09),
-                  builder: (ctx) => new Container(
-                    child: new FlutterLogo(),
-                  ),
-                ),
-              ],
+              markers: createMarkerForLocations(),
             ),
           ],
         ),
       ),
     );
   }
+
+  List<Marker> createMarkerForLocations() {
+    NumberFormat formatter = NumberFormat("#,###.#");
+    return weatherData.map((dataSet) =>
+        Marker(width: 31,
+            height: 44,
+            point: LatLng(dataSet.latitude, dataSet.longitude),
+          builder: (ctx) => new Container(
+            child: Image(image: AssetImage('res/images/marker.png')),
+          ), anchorPos: AnchorPos.align(AnchorAlign.top))).toList();
+  }
+
 }

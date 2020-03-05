f575e9cb5c6eb6043d4a619c03e01069657cf5f5
christian@voegtle.org <christian@voegtle.org>
2020-03-05 20:12:38 +0100

    Schöneres Icon

--- lib/view/infopage.dart
+++ lib/view/infopage.dart
@@ -57,7 +57,7 @@ class InfoPage extends StatelessWidget {
                               "https://github.com/cvoegtle/flutter_wetterwolke/blob/master/HELP.md")),
                   Divider(),
                   Caption("Versions Information"),
-                  Text("Version 1.13.21-21 vom 3.3.2020 ")
+                  Text("Version 1.13.22-22 vom 4.3.2020 ")
                 ]))));
   }
 }
